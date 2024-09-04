fc041b123f4073a66a234d76033656698a027849
dynamicsamic <mirabyansr@gmail.com>
2024-09-04 15:06:02 +0300

    feat: implement `add_producer` handler

--- src/restapi/handlers/producer.py
+++ src/restapi/handlers/producer.py
@@ -1,17 +1,23 @@
 from datetime import datetime
 from typing import Annotated
 
-from litestar import Router, delete, get, patch, post
-from litestar.contrib.pydantic import PydanticDTO
+from litestar import Request, Router, delete, get, patch, post
 from litestar.di import Provide
+from litestar.exceptions import HTTPException
 from litestar.params import Dependency
 
 from service.services import ProducerService
 from src.data.providers import provide_db_session
-from src.domain.models import MedicationProducer
+from src.domain.models import CreateProducer, Producer
+from src.service.exceptions import (
+    DuplicateError,
+    ExtraArgumentError,
+    InvalidArgumentTypeError,
+)
 from src.service.providers import provide_producer_service
 
-from ..query_filters import (
+from ..dto import CreateProducerDTO, ProducerDTO
+from ..query_params import (
     CreatedAfterParam,
     CreatedBeforeParam,
     IdsParam,
@@ -22,10 +28,8 @@ from ..query_filters import (
     UpdatedBeforeParam,
 )
 
-ProducerDTO = PydanticDTO[MedicationProducer]
 
-
-@get("/")
+@get("/", return_dto=ProducerDTO)
 async def list_producers(
     service: Annotated[ProducerService, Dependency(skip_validation=True)],
     limit: int | None = PageSizeParam,
@@ -36,8 +40,8 @@ async def list_producers(
     updated_after: datetime | None = UpdatedAfterParam,
     pks: list[int] | None = IdsParam,
     names: list[str] | None = NamesParam,
-) -> list[MedicationProducer]:
-    return await service.get_producers(
+) -> list[Producer]:
+    return await service.list(
         limit=limit,
         offset=offset,
         created_before=created_before,
@@ -49,9 +53,45 @@ async def list_producers(
     )
 
 
-@post("/")
-async def add_producer() -> None:
-    pass
+@post("/", dto=CreateProducerDTO, return_dto=ProducerDTO)
+async def add_producer(
+    request: Request,
+    service: Annotated[ProducerService, Dependency(skip_validation=True)],
+    data: CreateProducer,
+) -> Producer:
+    try:
+        producer = await service.create(data)
+    except DuplicateError as err:
+        request.logger.info(
+            f"Attempt to create duplicate instance failed with error: {err}"
+        )
+        raise HTTPException(
+            status_code=400, detail="Producer with provided data already exists"
+        )
+    except InvalidArgumentTypeError as err:
+        request.logger.info(
+            f"Invalid argument type detected, causing db query fail with error: {err}"
+        )
+        raise HTTPException(
+            status_code=400,
+            detail=(
+                "At least one of the provided arguments has incompatible "
+                "type with database representation"
+            ),
+        )
+    except ExtraArgumentError as err:
+        request.logger.info(
+            f"Extra argument detected, causing db query fail with error: {err}"
+        )
+        raise HTTPException(
+            status_code=400,
+            detail=(
+                "At least one of the provided arguments does not belong to "
+                "the database representation"
+            ),
+        )
+
+    return producer
 
 
 @get("/{prod_id: int}")

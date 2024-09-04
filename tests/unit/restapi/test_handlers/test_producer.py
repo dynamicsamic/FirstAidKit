5393a7766176f19ac13ad18c39250badea5c6ab2
dynamicsamic <mirabyansr@gmail.com>
2024-09-04 15:08:57 +0300

    test: add tests for `add_producer` handler; refactor tests for `list_producers` nandler

--- tests/unit/restapi/test_handlers/test_producer.py
+++ tests/unit/restapi/test_handlers/test_producer.py
@@ -5,26 +5,20 @@ import pytest_asyncio
 from litestar import status_codes
 from litestar.testing import AsyncTestClient
 
-from src.domain.models import MedicationProducer
+from src.domain.models import CreateProducer, Producer
 from src.restapi.app import app
+from src.service.exceptions import DuplicateError
 from src.service.services import ProducerService
 from src.utils import now as now_
 from tests.conftest import DEFAULT_LIMIT
+from tests.utils import convert_to_producer
 
 pytestmark = pytest.mark.asyncio
 
+
 now = now_()
-producer = MedicationProducer(
-    producer_id=1, name="test1", created_at=now, updated_at=now
-)
-default_filters = {
-    "pk": None,
-    "name": None,
-    "created_before": None,
-    "created_after": None,
-    "updated_before": None,
-    "updated_after": None,
-}
+producer = Producer(pk=1, name="test1", created_at=now, updated_at=now)
+producers = [producer for _ in range(5)]
 
 
 @pytest_asyncio.fixture(autouse=True, loop_scope="session")
@@ -36,7 +30,7 @@ async def test_client():
 class TestProducerHandlers:
     list_create_url = "/api/v1/producers"
 
-    @patch.object(ProducerService, "get_producers", return_value=[producer])
+    @patch.object(ProducerService, "list", return_value=producers)
     async def test_list_producers_without_query_args_uses_default_values(
         self, mock: AsyncMock, test_client: AsyncTestClient
     ):
@@ -55,9 +49,9 @@ class TestProducerHandlers:
         )
         body = resp.json()
         assert isinstance(body, list)
-        assert all(MedicationProducer.model_validate(item) for item in body)
+        assert all(Producer.model_validate(convert_to_producer(item)) for item in body)
 
-    @patch.object(ProducerService, "get_producers", return_value=[producer])
+    @patch.object(ProducerService, "list", return_value=producers)
     async def test_list_producers_with_query_args_uses_provided_args(
         self, mock: AsyncMock, test_client: AsyncTestClient
     ):
@@ -90,9 +84,9 @@ class TestProducerHandlers:
         )
         body = resp.json()
         assert isinstance(body, list)
-        assert all(MedicationProducer.model_validate(item) for item in body)
+        assert all(Producer.model_validate(convert_to_producer(item)) for item in body)
 
-    @patch.object(ProducerService, "get_producers", return_value=[])
+    @patch.object(ProducerService, "list", return_value=[])
     async def test_list_producers_with_empty_service_response(
         self, mock: AsyncMock, test_client: AsyncTestClient
     ):
@@ -102,7 +96,7 @@ class TestProducerHandlers:
         mock.assert_awaited_once()
         assert resp.json() == []
 
-    @patch.object(ProducerService, "get_producers", return_value=[producer])
+    @patch.object(ProducerService, "list", return_value=producers)
     async def test_list_producers_with_extra_query_arg_is_ignored(
         self, mock: AsyncMock, test_client: AsyncTestClient
     ):
@@ -112,7 +106,7 @@ class TestProducerHandlers:
         mock.assert_awaited_once()
         assert "extra" not in mock.await_args.kwargs
 
-    @patch.object(ProducerService, "get_producers")
+    @patch.object(ProducerService, "list")
     async def test_list_producers_with_invalid_query_arg_type_returns_error(
         self, mock: AsyncMock, test_client: AsyncTestClient
     ):
@@ -123,7 +117,7 @@ class TestProducerHandlers:
         body = resp.json()
         assert set(body.keys()) == {"status_code", "detail", "extra"}
 
-    @patch.object(ProducerService, "get_producers")
+    @patch.object(ProducerService, "list")
     async def test_list_producers_with_negative_limit_return_error(
         self, mock: AsyncMock, test_client: AsyncTestClient
     ):
@@ -134,7 +128,7 @@ class TestProducerHandlers:
         body = resp.json()
         assert set(body.keys()) == {"status_code", "detail", "extra"}
 
-    @patch.object(ProducerService, "get_producers")
+    @patch.object(ProducerService, "list")
     async def test_list_producers_with_limit_too_high_return_error(
         self, mock: AsyncMock, test_client: AsyncTestClient
     ):
@@ -144,3 +138,47 @@ class TestProducerHandlers:
         mock.assert_not_awaited()
         body = resp.json()
         assert set(body.keys()) == {"status_code", "detail", "extra"}
+
+    @patch.object(ProducerService, "create", return_value=producer)
+    async def test_add_producer_with_valid_payload_return_created_instance(
+        self, mock: AsyncMock, test_client: AsyncTestClient
+    ):
+        data = {"name": "hello"}
+        resp = await test_client.post(self.list_create_url, json=data)
+
+        assert resp.status_code == status_codes.HTTP_201_CREATED
+        mock.assert_awaited_once_with(CreateProducer(**data))
+        normalized_producer = convert_to_producer(resp.json())
+        assert Producer.model_validate(normalized_producer)
+
+    @patch.object(ProducerService, "create")
+    async def test_add_producer_with_invalid_payload_return_error(
+        self, mock: AsyncMock, test_client: AsyncTestClient
+    ):
+        resp = await test_client.post(self.list_create_url, json={"name": 123})
+        assert resp.status_code == status_codes.HTTP_400_BAD_REQUEST
+        mock.assert_not_awaited()
+
+    @patch.object(ProducerService, "create")
+    async def test_add_producer_with_empty_payload_return_error(
+        self, mock: AsyncMock, test_client: AsyncTestClient
+    ):
+        resp = await test_client.post(self.list_create_url, json={})
+        assert resp.status_code == status_codes.HTTP_400_BAD_REQUEST
+        mock.assert_not_awaited()
+
+    @patch.object(ProducerService, "create")
+    async def test_add_producer_with_extra_payload_return_error(
+        self, mock: AsyncMock, test_client: AsyncTestClient
+    ):
+        resp = await test_client.post(self.list_create_url, json={"extra": "extra"})
+        assert resp.status_code == status_codes.HTTP_400_BAD_REQUEST
+        mock.assert_not_awaited()
+
+    @patch.object(ProducerService, "create", side_effect=DuplicateError)
+    async def test_add_producer_with_duplicate_payload_return_error(
+        self, mock: AsyncMock, test_client: AsyncTestClient
+    ):
+        resp = await test_client.post(self.list_create_url, json={"name": "new_name"})
+        assert resp.status_code == status_codes.HTTP_400_BAD_REQUEST
+        mock.assert_awaited_once()

0aee68f0f9a8668a4803b168c7a18db276aa213b
dynamicsamic <mirabyansr@gmail.com>
2024-09-10 15:24:03 +0300

    test: complete producer service test suite

--- tests/unit/service/test_services/test_producer.py
+++ tests/unit/service/test_services/test_producer.py
@@ -1,14 +1,30 @@
+from typing import Any
 from unittest.mock import AsyncMock, patch
 
 import pytest
+from pydantic import BaseModel
+from sqlalchemy.exc import CompileError, DBAPIError, IntegrityError
 
 from src.data.models import Producer as DBProducer
 from src.data.repository import ProducerRepository as Repo
-from src.domain.models import Producer
+from src.domain.models import CreateProducer, Producer
+from src.service.exceptions import (
+    DuplicateError,
+    ExtraArgumentError,
+    InvalidArgumentTypeError,
+)
 from src.service.services import ProducerService as Service
 from src.utils import now as now_
 
 pytestmark = pytest.mark.asyncio
+
+
+class MockProducer(BaseModel):
+    name: Any = None
+    extra: Any = None
+    created_at: Any = None
+    updated_at: Any = None
+
 
 now = now_()
 producer_id = 1
@@ -88,11 +104,11 @@ async def test_list_with_empty_db_response_return_empty_list(
 @patch.object(Repo, "fetch_many")
 async def test_list_items_with_invalid_column_name_filter_ignored(mock: AsyncMock):
     r = await Service(None).list_items(limit=1, offset=1, invalid="invalid")
-
     mock.assert_awaited_once()
     assert "invalid" not in mock.await_args.kwargs
     assert isinstance(r, list)
     assert all(isinstance(item, Producer) for item in r)
+
 
 @pytest.mark.xfail(raises=ValueError, strict=True)
 @patch.object(Repo, "fetch_many")
@@ -100,20 +116,17 @@ async def test_list_items_with_invalid_column_type_filter_ignored(mock: AsyncMoc
     await Service(None).list_items(limit=1, offset=1, pk=["one", "two"])
     mock.assert_awaited_once()
 
+
 @pytest.mark.xfail(raises=ValueError, strict=True)
 @patch.object(Repo, "fetch_many")
-async def test_list_with_zero_limit_raises_value_error(
-    mock: AsyncMock,
-):
+async def test_list_with_zero_limit_raises_value_error(mock: AsyncMock):
     await Service(None).list_items(limit=0, offset=1)
     mock.assert_not_awaited()
 
 
 @pytest.mark.xfail(raises=ValueError, strict=True)
 @patch.object(Repo, "fetch_many")
-async def test_list_with_limit_too_high_raises_value_error(
-    mock: AsyncMock,
-):
+async def test_list_with_limit_too_high_raises_value_error(mock: AsyncMock):
     from src.settings import settings
 
     await Service(None).list_items(limit=settings.PAGINATION_LIMIT + 1, offset=1)
@@ -122,14 +135,104 @@ async def test_list_with_limit_too_high_raises_value_error(
 
 @pytest.mark.xfail(raises=ValueError, strict=True)
 @patch.object(Repo, "fetch_many")
-async def test_list_with_negative_offset_raises_value_error(
-    mock: AsyncMock,
-):
+async def test_list_with_negative_offset_raises_value_error(mock: AsyncMock):
     await Service(None).list_items(limit=1, offset=-1)
     mock.assert_not_awaited()
 
 
+@patch.object(Repo, "insert_one", return_value=producer)
+async def test_create_with_valid_payload_return_created_instance(mock: AsyncMock):
+    data = {"name": "test"}
+    r = await Service(None).create(CreateProducer(**data))
+    mock.assert_awaited_once_with(**data)
+    assert isinstance(r, Producer)
+
+
+@pytest.mark.xfail(raises=ExtraArgumentError, strict=True)
+@patch.object(Repo, "insert_one", side_effect=CompileError)
+async def test_create_with_extra_attribute_raises_extra_arg_error(_):
+    await Service(None).create(MockProducer(**{"name": "test", "extra": "extra"}))
+
+
+@pytest.mark.xfail(raises=InvalidArgumentTypeError, strict=True)
+@patch.object(Repo, "insert_one", side_effect=DBAPIError)
+async def test_create_with_invalid_attribute_type_raises_invalid_arg_type_error(_):
+    await Service(None).create(MockProducer(name=1234))
+
+
+@pytest.mark.xfail(raises=ValueError, strict=True)
+@patch.object(Repo, "insert_one")
+async def test_create_with_empty_payload_raises_value_error(_):
+    await Service(None).create(MockProducer())
+
+
+@pytest.mark.xfail(raises=DuplicateError, strict=True)
+@patch.object(Repo, "insert_one", side_effect=IntegrityError(None, None, Exception))
+async def test_create_duplicate_payload_raises_duplicate_error(_):
+    await Service(None).create(MockProducer(name="test"))
+
+
 @patch.object(Repo, "fetch_one_by_pk", return_value=producer)
-async def test_get_return_producer_object(mock: AsyncMock):
-    p = await Service(None).get(producer_id)
-    assert isinstance(p, Producer)
+async def test_get_with_existing_id_return_producer_object(mock: AsyncMock):
+    r = await Service(None).get(producer_id)
+    mock.assert_awaited_once_with(producer_id)
+    assert isinstance(r, Producer)
+
+
+@patch.object(Repo, "fetch_one_by_pk", return_value=None)
+async def test_get_with_id_does_not_exist_return_none(mock: AsyncMock):
+    r = await Service(None).get(producer_id)
+    mock.assert_awaited_once_with(producer_id)
+    assert r is None
+
+
+@patch.object(Repo, "update_one_by_pk", return_value=producer)
+async def test_update_with_valid_partial_data_return_producer_object(mock: AsyncMock):
+    data = {"created_at": now}
+    r = await Service(None).update(producer_id, MockProducer(**data))
+    mock.assert_awaited_once_with(producer_id, **data)
+    assert isinstance(r, Producer)
+
+
+@patch.object(Repo, "update_one_by_pk", return_value=producer)
+async def test_update_with_valid_complete_data_return_producer_object(mock: AsyncMock):
+    data = {"name": "test", "created_at": now, "updated_at": now}
+    r = await Service(None).update(producer_id, MockProducer(**data))
+    mock.assert_awaited_once_with(producer_id, **data)
+    assert isinstance(r, Producer)
+
+
+@patch.object(Repo, "update_one_by_pk", return_value=None)
+async def test_update_with_id_does_not_exist_return_none(mock: AsyncMock):
+    data = {"name": "test"}
+    r = await Service(None).update(producer_id, MockProducer(**data))
+    mock.assert_awaited_once_with(producer_id, **data)
+    assert r is None
+
+
+@pytest.mark.xfail(raises=ExtraArgumentError, strict=True)
+@patch.object(Repo, "update_one_by_pk", side_effect=CompileError)
+async def test_update_with_extra_attribute_raises_(mock: AsyncMock):
+    await Service(None).update(producer_id, MockProducer(extra="extra"))
+
+
+@pytest.mark.xfail(raises=InvalidArgumentTypeError, strict=True)
+@patch.object(Repo, "update_one_by_pk", side_effect=DBAPIError(None, None, Exception))
+async def test_update_with_invalid_attribute_type_raises_invalid_arg_type_error(
+    mock: AsyncMock,
+):
+    await Service(None).update(producer_id, MockProducer(name=4773))
+
+
+@pytest.mark.xfail(raises=DuplicateError, strict=True)
+@patch.object(
+    Repo, "update_one_by_pk", side_effect=IntegrityError(None, None, Exception)
+)
+async def test_update_with_duplicate_payload_raises_duplicate_error(mock: AsyncMock):
+    await Service(None).update(producer_id, MockProducer(name="test"))
+
+
+@pytest.mark.xfail(raises=ValueError, strict=True)
+@patch.object(Repo, "update_one_by_pk")
+async def test_update_with_empty_data_raises_value_errror(mock: AsyncMock):
+    await Service(None).update(producer_id, MockProducer())
